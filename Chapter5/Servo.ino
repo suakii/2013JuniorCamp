e9412d2ee8dbbcba194496e6f6f6cf72d17672ae
suakii <suakii@gmail.com>
2013-07-24 00:38:09 +0900

    PWM을 통한 LED 제어 및 서보모터 사용하기 + LDR

---
+++ Chapter5/Servo.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+int servoPin = 9;
+Servo servo;
+int angle = 0;
+
+void setup() {
+  servo.attach(servoPin);//
+}
+void loop() {
+   for (angle = 0; angle < 180; angle++) {
+     servo.write(angle);
+     delay(15);
+   }    
+   for (angle = 180; angle > 0; angle--) {
+     servo.write(angle);
+     delay(15);
+   }
+}
