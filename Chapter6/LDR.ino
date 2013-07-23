23bd3b8430b8f6742baf73eb7c0f818e791e7e5a
suakii <suakii@gmail.com>
2013-07-24 00:39:09 +0900

    빛의 양을 측정해보기 및 프로세싱을 이용한 빛의 시각화

---
+++ Chapter6/LDR.ino
@@ -0,0 +1,17 @@
+int sensorPin = 0; 
+int ledPin = 6; 
+int sensorValue = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);    
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);    
+  Serial.println(sensorValue);
+  digitalWrite(ledPin, HIGH);  
+  delay(sensorValue);
+  digitalWrite(ledPin, LOW);  
+  delay(sensorValue);
+}
